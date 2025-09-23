24ad43b7b6a2462d1771f404cf624dbeb065a82d
Diego Vargas <diego.vargasd@gmail.com>
2025-09-23 16:52:28 -0600

    Completed ConnectFour minimum assignment

--- Program.cs
+++ Program.cs
@@ -6,6 +6,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+builder.Services.AddSingleton<GameState>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
